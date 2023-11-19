9c34d966e8a911f1eea1bc6fe1b444762b4a4c59
M4RKL4B0Y <markvlaboy@gmail.com>
2023-11-18 22:18:01 -0500

    add js

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+function newImage(url){
+    let image = document.createElement('img')
+    image.src = url
+    document.body.append(image)
+    return image
+}
+
+function move(image){
+    image.style.position = 'fixed'
+    
+
+    function moveToCoordinates (left, bottom) {
+        image.style.left = left + 'px'
+        image.style.bottom = bottom + 'px'
+    }
+    return {
+        to: moveToCoordinates
+    }
+}
+
+
+
+
+
 function newImage(url, left, bottom){
     let object = document.createElement('img')
     object.src = url
@@ -7,6 +31,7 @@ function newImage(url, left, bottom){
     document.body.append(object)
     return object
 }
+
 
 function newItem(url, left, bottom){
     let item = newImage(url, left, bottom)
@@ -18,10 +43,10 @@ function newItem(url, left, bottom){
     })
 }
 
-
-
+let inventory = document.createElement('div')
+  
 function newInventory(){
-    let inventory = document.createElement('div')
+  
     inventory.style.position = 'fixed'
     inventory.style.bottom = '0px';
     inventory.style.left = '0px'
@@ -34,18 +59,21 @@ function newInventory(){
     inventory.style.border = '2px solid black'
     inventory.style.backgroundColor = 'brown'
     document.body.append(inventory)
-
-    return inventory
 }
 
 newInventory()
-newImage('assets/green-character.gif', 100, 250)
-newImage('assets/tree.png', 200, 450)
-newImage('assets/pillar.png', 350, 250)
-newImage('assets/pine-tree.png', 450, 350)
-newImage('assets/crate.png', 150, 350)
-newImage('assets/well.png', 500, 575)
+move(newImage('assets/green-character.gif')).to(900,250)
+move(newImage('assets/tree.png')).to(100, 650)
+move(newImage('assets/pillar.png')).to(750, 250)
+move(newImage('assets/pine-tree.png')).to(750, 650)
+move(newImage('assets/crate.png')).to(150, 150)
+move(newImage('assets/well.png')).to(400, 575)
+move(newImage('assets/boulder.png')).to(300,300)
 
 newItem('assets/sword.png', 500, 555)
 newItem('assets/shield.png', 165, 335)
 newItem('assets/staff.png', 600, 250)
+
+
+
+
